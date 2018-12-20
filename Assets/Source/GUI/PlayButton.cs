4808f814e83b231114e550144fb5ff9d3df87e05
JejeG <garciajeromejeje@hotmail.fr>
2018-12-20 21:02:38 +0100

    quick end game (to polish)

--- Assets/Source/GUI/PlayButton.cs
+++ Assets/Source/GUI/PlayButton.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         Button b = gameObject.GetComponent<Button>();
-        b.onClick.AddListener(delegate () { GameManager.Instance.RestartGame(); });
+
+        if(SceneManager.GetActiveScene().name == "menu") {
+        	b.onClick.AddListener(delegate () { GameManager.Instance.RestartGame(); });
+        } else {
+        	b.onClick.AddListener(delegate () {
+        		if(GameManager.isEnd == true) {
+        			GameManager.Instance.RestartGame(); 	
+        		} else {
+        			GameManager.Instance.IncreaseLevel();
+        			Text buttonText = b.GetComponentInChildren<Text>();
+        			buttonText.text = "Next level";
+        		}
+        	});
+        }
     }
 }
